0e110d6355e5733af328180d13cc255b5e46fbad
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-03-03 11:45:06 +0100

    Fixed equality test once again 🤡

--- test/good/EqualCheck.java
+++ test/good/EqualCheck.java
@@ -1,10 +1,16 @@
 public class EqualCheck {
 
       public static void main(String[] args) {
-        int a = 1;
-        int b = 1;
-        boolean res = (a == b);
-        System.out.println(res);
+        System.out.println(new C().test());
     }
   
+}
+
+class C {
+  public boolean test() {
+      int a = 1;
+      int b = 1;
+      boolean res = (a==b);
+      return res;
+  }
 }
