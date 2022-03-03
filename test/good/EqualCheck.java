dd6c142a6288cdf2e402d90e582f8c4b08ff6c33
Dorian Bucaille <dorian.bucaille@orange.fr>
2022-03-03 11:52:57 +0100

    Really fixed equality check 😔

--- test/good/EqualCheck.java
+++ test/good/EqualCheck.java
@@ -1,15 +1,32 @@
-public class EqualCheck {
-
-      public static void main(String[] args) {
-        System.out.println(new C().test());
-    }
+class EqualCheck {
+  public static void main(String[] args) {
+      System.out.println(new TestEqual().test());
+  }
 }
+class ClasseAlpha {
 
-class C {
-  public boolean test() {
-      int a = 1;
-      int b = 1;
-      boolean res = a==b;
-      return res;
+}
+class ClasseBeta extends ClasseAlpha {
+
+}
+class ClasseCharlie extends ClasseAlpha {
+
+}
+class TestEqual {
+  public int test() {
+      ClasseBeta beta;
+      ClasseAlpha betaToAlpha;
+      ClasseAlpha alpha;
+      int resultat;
+      beta = new ClasseBeta();
+      betaToAlpha = beta;
+      alpha = new ClasseAlpha();
+
+      if (!(alpha == beta) && 1 == 1 && (beta == betaToAlpha) && !(alpha == betaToAlpha)) {
+          resultat = 1;
+      } else {
+          resultat = 0;
+      }
+      return resultat;
   }
 }
